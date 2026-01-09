f9a744da8e677140e353108ebc19369a4adbe0fd
John Lahut <lahut13@gmail.com>
2026-01-09 06:22:58 -0500

    init

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const config = {
+  trailingComma: 'all',
+  singleQuote: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: [''],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
+
+export default config;
